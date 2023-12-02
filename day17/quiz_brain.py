5d4c04eb34fb0e7903fda2b843e99e5aff9dc19e
Hemanth Sai Chinthalapudi <91429511+hemanthsaich@users.noreply.github.com>
2023-12-02 13:41:07 +0530

    Update quiz_brain.py

--- day17/quiz_brain.py
+++ day17/quiz_brain.py
@@ -1,39 +1,25 @@
 class QuizBrain:
-    def __init__(self, questions_list):
-        self.question_number = 0
-        self.questions_list = questions_list
-        self.score = 0
 
-    def next_question(self):
-        """Displays and processes the next question."""
-        # the Q number in the text needs to start from 1
-        print(f"Q.{self.question_number + 1}: {self.questions_list[self.question_number].text} (True/False)?")
-        while True:
-            choice = input("> ").lower()
-            if choice in ["true", "false"]:
-                break
-            print(f"Invalid choice. Please type \"True\", \"False\".")
-        self.check_answer(choice, self.questions_list[self.question_number].answer)
-        # only increase this once the current question is completed
-        self.question_number += 1
+    def __init__(self, q_list):
+        self.question_number = 0
+        self.score = 0
+        self.question_list = q_list
 
     def still_has_questions(self):
-        """Checks if there are questions remaining and returns a BOOL."""
-        if self.question_number < len(self.questions_list):
-            return True
-        return False
+        return self.question_number < len(self.question_list)
 
-    def check_answer(self, choice, answer):
-        """Takes two STR and compares them, then prints the result and score."""
-        if choice == answer.lower():
-            print("You got it right!")
+    def next_question(self):
+        current_question = self.question_list[self.question_number]
+        self.question_number += 1
+        user_answer = input(f"Q.{self.question_number}: {current_question.text} (True/False): ")
+        self.check_answer(user_answer, current_question.answer)
+
+    def check_answer(self, user_answer, correct_answer):
+        if user_answer.lower() == correct_answer.lower():
             self.score += 1
+            print("You got it right!")
         else:
             print("That's wrong.")
-        # show the correct answer either way
-        print(f"The correct answer was: {answer}.")
-        # show the number of correct / total answers
-        # again it needs + 1 since the printed value starts with 1
-        print(f"Your current score is: {self.score}/{self.question_number + 1}.")
-        # print an empty line between the questions
-        print()
+        print(f"The correct answer was: {correct_answer}.")
+        print(f"Your current score is: {self.score}/{self.question_number}")
+        print("\n")

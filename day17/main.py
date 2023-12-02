2b9fd674ecf6a8779b5b74eb5f9a4effd2719244
Hemanth Sai Chinthalapudi <91429511+hemanthsaich@users.noreply.github.com>
2023-12-02 13:39:47 +0530

    Update main.py

--- day17/main.py
+++ day17/main.py
@@ -1,25 +1,18 @@
-# Quiz
-
-import data
 from question_model import Question
+from data import question_data
 from quiz_brain import QuizBrain
 
-
-# switch to True to use the original questions
-use_original = False
-
 question_bank = []
-# fill up the question bank list with Question objects
-if use_original:
-    for dic in data.original_question_data:
-        question_bank.append(Question(dic["text"], dic["answer"]))
-else:
-    for dic in data.question_data:
-        question_bank.append(Question(dic["question"], dic["correct_answer"]))
+for question in question_data:
+    question_text = question["question"]
+    question_answer = question["correct_answer"]
+    new_question = Question(question_text, question_answer)
+    question_bank.append(new_question)
 
-qb = QuizBrain(question_bank)
-# repeat while there are still questions remaining
-while qb.still_has_questions():
-    qb.next_question()
-# no need for a + 1 to the question_number here, as it gets increased after the last question is completed
-print(f"Your final score is: {qb.score}/{qb.question_number}.")
+quiz = QuizBrain(question_bank)
+
+while quiz.still_has_questions():
+    quiz.next_question()
+
+print("You've completed the quiz")
+print(f"Your final score was: {quiz.score}/{quiz.question_number}")

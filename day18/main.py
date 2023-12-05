d973703065dcf65679a196e4dbc8c27b83d31383
Hemanth Sai Chinthalapudi <91429511+hemanthsaich@users.noreply.github.com>
2023-12-05 09:19:14 +0530

    Update main.py

--- day18/main.py
+++ day18/main.py
@@ -1,58 +1,45 @@
 # Hirst Painting
 
-import turtle
+'''Part-1 Extract colors from image'''
+#import colorgram
+# rgb_color = []
+# colors = colorgram.extract('download.jpg',20)
+# for color in colors:
+#     r = color.rgb.r
+#     g = color.rgb.g
+#     b = color.rgb.b
+#     new_color =(r,g,b)
+#     rgb_color.append(new_color)
+# print(rgb_color)
+
+'''Part-2 Drawing dots'''
+import turtle as ts
 import random
+ts.colormode(255)
 
-import colorgram
+chin = ts.Turtle()
+chin.speed('fastest')
+chin.penup()
+chin.hide()
 
+colors_list = [(243, 220, 103), (229, 72, 96), (226, 126, 153), (17, 116, 180), (5, 181, 222), (102, 182, 219), (8, 185, 138), (241, 161, 180), (245, 163, 151), (213, 91, 77), (108, 205, 154), (139, 218, 194), (171, 51, 97), (14, 136, 87)]
 
-def paint(cols, rows):
-    """Takes number of columns and rows as INT and paints a dot of random color at each position."""
-    # extract the colors
-    extracted_colors = colorgram.extract("image.jpg", 30)
-    color_list = []
-    for col in extracted_colors:
-        color_list.append(col.rgb)
+chin.setheading(225)
+chin.forward(300)
+chin.setheading(0)
 
-    # set up the brush
-    brush = turtle.Turtle()
-    brush.shape("circle")
-    # from a range of 0-10, 7 seems reasonable
-    brush.speed(7)
-    brush.penup()
-    brush.hideturtle()
+no_of_dots = 100
+for dot_count in range(1, no_of_dots + 1):
+    chin.dot(20, random.choice(colors_list))
+    chin.forward(50)
 
-    # initial position
-    width = screen.window_width()
-    height = screen.window_height()
-    # offset between the dots
-    x_offset = int(width / cols)
-    y_offset = int(height / rows)
-    # the coordinates go from -300 to +300 (i.e. half the height or width)
-    # initial offset by half of the distance between each dot
-    x_pos = int(width / -2 + x_offset / 2)
-    y_pos = int(height / -2 + y_offset / 2)
-    for _ in range(0, rows):
-        for __ in range(0, cols):
-            brush.setx(x_pos)
-            brush.sety(y_pos)
-            brush.dot(30, random.choice(color_list))
-            x_pos += x_offset
-        # reset the x position to the first column
-        x_pos = int(width / -2 + x_offset / 2)
-        y_pos += y_offset
+    if dot_count % 10 == 0:
+        chin.setheading(90)
+        chin.forward(50)
+        chin.setheading(180)
+        chin.forward(500)
+        chin.setheading(0)
 
 
-# set the color mode to accept RGB values instead of color name strings
-turtle.colormode(255)
-# set up the screen
-screen = turtle.Screen()
-screen.title("Hirst-like art")
-# possible to modify the canvas size
-screen.setup(width=800, height=600)
-
-# paint the image, the number of columns and rows can be any (reasonable) positive INT
-paint(cols=6, rows=4)
-
-# make the window wait for a click, rather than closing immediately after completion
+screen = ts.Screen()
 screen.exitonclick()

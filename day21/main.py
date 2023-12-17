84e5aa3240f9ad47d1c0d8b5341fe2e32a3f6595
Hemanth Sai Chinthalapudi <91429511+hemanthsaich@users.noreply.github.com>
2023-12-17 13:58:53 +0530

    Create main.py

--- day21/main.py
+++ day21/main.py
@@ -1,77 +1,50 @@
 # Snake Game Part 2
 
-import turtle
-import time
-
+from turtle import Screen, Turtle
 from snake import Snake
 from food import Food
 from scoreboard import Scoreboard
+import time
 
-
-# set the speed of the game as INT, values from 1 (slowest) to 10 (fastest)
-# it's more user-friendly this way
-GAME_SPEED = 8
-# dimensions for the game window
-WIDTH = 800
-HEIGHT = 600
-
-# convert the game speed to milliseconds
-# 1-10 becomes 333ms-33ms
-wait_time = (11 - GAME_SPEED) / 30
-# calculate the borders, with the snake's width (20) being the size of a "grid"
-# allow half a width for leeway, for possible issues caused by rounding (as below)
-x_range = int(WIDTH / 2) - 10
-y_range = int(HEIGHT / 2) - 10
-
-
-# screen setup
-screen = turtle.Screen()
-screen.setup(width=WIDTH, height=HEIGHT)
-screen.title("Finished Snake Game")
+screen = Screen()
+screen.setup(height=600, width=600)
 screen.bgcolor("black")
-# disable automatic screen updates
-screen.tracer(0)
+screen.title("Snake Game")
+screen.tracer()
 
-# snake setup
+segments = []
+
 snake = Snake()
 food = Food()
 scoreboard = Scoreboard()
 
-# controls setup
 screen.listen()
 screen.onkey(snake.up, "Up")
 screen.onkey(snake.down, "Down")
 screen.onkey(snake.left, "Left")
 screen.onkey(snake.right, "Right")
 
-game_is_over = False
-while not game_is_over:
-    # update the screen
+game_is_on = True
+while game_is_on:
     screen.update()
-    # to control the speed, wait for the set amount of time
-    time.sleep(wait_time)
+    time.sleep(0.1)
     snake.move()
 
-    # detect collision with food, with a little leeway
-    # to allow for minor differences due to rounding when converting the random coordinates to INT
-    # likely requires more testing with a wide range of screen sizes, but 10 seems to work as intended
-    if snake.head.distance(food) < 10:
-        scoreboard.add_one()
+    # Detect collision with food
+    if snake.head.distance(food) < 15:
+        food.refresh()
         snake.extend()
-        food.respawn()
+        scoreboard.increase_score()
 
-    # detect collision with wall
-    if snake.head.xcor() < -x_range or snake.head.xcor() > x_range \
-            or snake.head.ycor() < -y_range or snake.head.ycor() > y_range:
-        game_is_over = True
+    # Detect collisions with wall
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_is_on = False
+        scoreboard.game_over()
 
-    # detect collision with any segment except the head (i.e. except i = 0)
-    for i in range(1, snake.length):
-        if snake.head.distance(snake.segments[i]) < 10:
-            game_is_over = True
+    # Detect collision with tail
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
 
-# display the game over message
-scoreboard.game_over()
-
-# wait for click before closing the window
 screen.exitonclick()

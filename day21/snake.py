fefa8d715437a66f4be4a93d55fc7bcc4b3f9628
Hemanth Sai Chinthalapudi <91429511+hemanthsaich@users.noreply.github.com>
2023-12-17 14:00:00 +0530

    Create snake.py

--- day21/snake.py
+++ day21/snake.py
@@ -1,6 +1,6 @@
-import turtle
+from turtle import Turtle
 
-START_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
 MOVE_DISTANCE = 20
 UP = 90
 DOWN = 270
@@ -9,60 +9,45 @@ RIGHT = 0
 
 
 class Snake:
+
     def __init__(self):
         self.segments = []
         self.create_snake()
-        # to make the "head" easier to access
         self.head = self.segments[0]
-        self.length = len(self.segments)
 
     def create_snake(self):
-        """Creates a new snake made up by squares at locations set in the START_POSITIONS list."""
-        for i in range(len(START_POSITIONS)):
-            self.add_segment(i)
-            # set the positions as defined
-            self.segments[i].setpos(START_POSITIONS[i])
+        for position in STARTING_POSITIONS:
+            self.add_segment(position)
 
-    def add_segment(self, i):
-        # a simple append works for at beginning too, since we're starting with an empty list
-        self.segments.append(turtle.Turtle(shape="square"))
-        self.segments[i].color("white")
-        self.segments[i].penup()
+    def add_segment(self, position):
+        new_segment = Turtle("square")
+        new_segment.color("white")
+        new_segment.penup()
+        new_segment.goto(position)
+        self.segments.append(new_segment)
 
     def extend(self):
-        # the index of the new segment is equal to the current number of segments
-        self.add_segment(self.length)
-        # set the position of the new segment to the position of the previous one
-        self.segments[self.length].setpos(self.segments[self.length - 1].pos())
-        self.length += 1
+        self.add_segment(self.segments[-1].position())
 
     def move(self):
-        """Moves the snake forward by the distance set in MOVE_DISTANCE."""
-        # start from the end, go backwards by 1 with each step, up to the second segment (i.e. i = 1)
-        for i in range(self.length - 1, 0, -1):
-            # set the new location to the position of the previous segment
-            self.segments[i].setx(self.segments[i - 1].xcor())
-            self.segments[i].sety(self.segments[i - 1].ycor())
-        # move the first segment in whatever direction it's facing
+        for seg_num in range(len(self.segments) - 1, 0, -1):
+            new_x = self.segments[seg_num - 1].xcor()
+            new_y = self.segments[seg_num - 1].ycor()
+            self.segments[seg_num].goto(new_x, new_y)
         self.head.forward(MOVE_DISTANCE)
 
     def up(self):
-        """Turn the snake towards the top of the screen."""
-        # to avoid turning backwards
         if self.head.heading() != DOWN:
             self.head.setheading(UP)
 
     def down(self):
-        """Turn the snake towards the bottom of the screen."""
         if self.head.heading() != UP:
             self.head.setheading(DOWN)
 
     def left(self):
-        """Turn the snake towards the left side of the screen."""
         if self.head.heading() != RIGHT:
             self.head.setheading(LEFT)
 
     def right(self):
-        """Turn the snake towards the right side of the screen."""
         if self.head.heading() != LEFT:
             self.head.setheading(RIGHT)

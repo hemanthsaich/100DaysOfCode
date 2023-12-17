18fa1b501849c3f013b415151888a687c18c3c60
Hemanth Sai Chinthalapudi <91429511+hemanthsaich@users.noreply.github.com>
2023-12-17 13:59:31 +0530

    Create scoreboard.py

--- day21/scoreboard.py
+++ day21/scoreboard.py
@@ -1,35 +1,28 @@
-import turtle
+from turtle import Turtle
 
-FONT = ("Arial", 20, "normal")
+ALIGNMENT = "center"
+FONT = ("Courier", 24, "normal")
 
 
-class Scoreboard(turtle.Turtle):
+class Scoreboard(Turtle):
+
     def __init__(self):
-        """Creates a scoreboard and places it on the screen."""
         super().__init__()
-        self.color("silver")
-        self.hideturtle()
-        self.speed(0)
-        self.penup()
-        # the x coordinate can be a static 0 so it's centered
-        self.setpos(0, int(self.getscreen().window_height() / 2 - 30))
-        # set the initial score
         self.score = 0
-        self.update()
+        self.color("white")
+        self.penup()
+        self.goto(0, 270)
+        self.hideturtle()
+        self.update_scoreboard()
 
-    def update(self):
-        """Updates the scoreboard."""
-        # clear this "turtle" object before updating it
-        self.clear()
-        self.write(f"Score: {self.score}", align="center", font=FONT)
-
-    def add_one(self):
-        """Increases score by 1 and updates the scoreboard."""
-        self.score += 1
-        self.update()
+    def update_scoreboard(self):
+        self.write(f"Score : {self.score}", align=ALIGNMENT, font=FONT)
 
     def game_over(self):
-        """Displays "GAME OVER" message."""
-        # place it to the middle of the screen
-        self.setpos(0, 0)
-        self.write("GAME OVER", align="center", font=FONT)
+        self.goto(0, 0)
+        self.write("GAME OVER", align=ALIGNMENT, font=FONT)
+
+    def increase_score(self):
+        self.score += 1
+        self.clear()
+        self.update_scoreboard()

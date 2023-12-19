e8e5ac0ce7bab53f247e3ec3438cf00c46cfaf8d
Hemanth Sai Chinthalapudi <91429511+hemanthsaich@users.noreply.github.com>
2023-12-19 11:00:13 +0530

    Create scoreboard.py

--- day22/scoreboard.py
+++ day22/scoreboard.py
@@ -1,41 +1,31 @@
-import turtle
+from turtle import Turtle
 
-FONT = ("Arial", 60, "normal")
+ALIGNMENT = "center"
+FONT = ("Courier", 80, "normal")
 
 
-class Scoreboard(turtle.Turtle):
+class Scoreboard(Turtle):
+
     def __init__(self):
-        """Creates a scoreboard and places it on the screen."""
         super().__init__()
-        self.color("silver")
-        self.hideturtle()
-        self.speed(0)
+        self.color("white")
         self.penup()
-        # find the y coordinate, the x will be static
-        self.y_pos = int(self.getscreen().window_height() / 2 - 100)
-        # set the initial scores
-        self.left_score = 0
-        self.right_score = 0
-        self.update()
+        self.hideturtle()
+        self.l_score = 0
+        self.r_score = 0
+        self.update_scoreboard()
 
-    def update(self):
-        """Updates the scoreboard."""
+    def update_scoreboard(self):
         self.clear()
-        self.setpos(-80, self.y_pos)
-        self.write(self.left_score, align="center", font=FONT)
-        self.setpos(80, self.y_pos)
-        self.write(self.right_score, align="center", font=FONT)
+        self.goto(-100, 200)
+        self.write(self.l_score, align=ALIGNMENT, font=FONT)
+        self.goto(100, 200)
+        self.write(self.r_score, align=ALIGNMENT, font=FONT)
 
-    def add_one_left(self):
-        """Increases the score of the left player by 1 and updates the scoreboard."""
-        self.left_score += 1
-        self.update()
+    def l_point(self):
+        self.l_score += 1
+        self.update_scoreboard()
 
-    def add_one_right(self):
-        """Increases the score of the right player by 1 and updates the scoreboard."""
-        self.right_score += 1
-        self.update()
-
-    def game_over(self):
-        self.setpos(0, -self.y_pos)
-        self.write("GAME OVER", align="center", font=FONT)
+    def r_point(self):
+        self.r_score += 1
+        self.update_scoreboard()

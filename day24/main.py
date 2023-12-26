2d987228e2cb1048d41156ff0adb5ca78717db75
Hemanth Sai Chinthalapudi <91429511+hemanthsaich@users.noreply.github.com>
2023-12-26 09:07:47 +0530

    Update main.py

--- day24/main.py
+++ day24/main.py
@@ -1,40 +1,14 @@
 # Mail Merge
 
-LETTER_TEMPLATE = "./Input/Letters/starting_letter.txt"
-INVITED_NAMES = "./Input/Names/invited_names.txt"
-OUTPUT_FOLDER = "./Output/ReadyToSend/"
+PLACEHOLDER = "[name]"
 
+with open("./Input/Names/invited_names.txt") as names_file:
+    names = names_file.readlines()
 
-def load_template(text_file):
-    """Takes a text file and returns its content as a STR."""
-    with open(text_file, mode="r") as file:
-        letter_template = file.read()
-        return letter_template
-
-
-def load_names(text_file):
-    """Takes a text file and returns its lines in a LIST of STRs."""
-    with open(text_file, mode="r") as file:
-        names = []
-        for line in file:
-            # remove the new line character
-            names.append(line.strip("\n"))
-        return names
-
-
-def save_letter(content, file_name):
-    """Takes STRs and saves the content in a text file with the provided name."""
-    # set the relative path and file name with appropriate extension
-    output = OUTPUT_FOLDER + file_name + ".txt"
-    with open(output, mode="w") as file:
-        file.write(content)
-
-
-# load the template and the list of names
-template = load_template(LETTER_TEMPLATE)
-name_list = load_names(INVITED_NAMES)
-for name in name_list:
-    # replace the placeholder with a name
-    letter = template.replace("[name]", name)
-    # save as a new text file called name.txt
-    save_letter(letter, name)
+with open("./Input/Letters/starting_letter.txt") as letter_file:
+    letter_contents = letter_file.read()
+    for name in names:
+        stripped_name = name.strip()
+        new_letter = letter_contents.replace(PLACEHOLDER, stripped_name)
+        with open(f"./Output/ReadyToSend/letter_for_{stripped_name}.txt", mode = "w") as completed_letter:
+            completed_letter.write(new_letter)
